0ee5d17b0807358eb72628880ae5a485129b30ac
ManiRams65 <svvmanikandan@gmail.com>
2023-11-30 03:59:46 +0530

    Server URL change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api/activity";
+export const BASE_URL = "https://gamification-service.vercel.app/api/activity";
 export const ENDPOINTS = {
   getActivityDetailById: "/getActivityDetailById",
   getEmployeeActivity: "/employee-activity",
